d3524fe1d8360b72859d863aa38695896905a1d6
song.cpe <song.cpe@gmail.com>
2018-03-22 15:37:01 +0700

    added gitignore, db.json

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const users = require('./db/user')
 
 app.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+app.get('/getUsers', function(req, res) {
+    res.send(users)
 })
 
 app.listen(8888, ()=>{
