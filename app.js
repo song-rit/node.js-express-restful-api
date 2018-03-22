d5ba3330f0eb7dfe646f04c65b1ce86eaaf9f5fe
song.cpe <song.cpe@gmail.com>
2018-03-22 17:24:30 +0700

    find by id

--- app.js
+++ app.js
@@ -7,9 +7,29 @@ app.get('/', (req, res) => {
 })
 
 app.get('/getUsers', function(req, res) {
-    res.send(users)
+    res.json(users)
 })
 
-app.listen(8888, ()=>{
+app.get('/getUserById/:id', (req, res) => {
+    // res.json(users.find(user => user.id === req.params.id))
+    var id = req.params.id
+    res.json(users.find(user => user.id === id))
+})
+
+app.get('/getUserByParam', (req, res) => {
+    // wait for request param from get method
+    var id = req.query.id
+    
+    // this is older format
+    //var id = req.body('id')
+
+    res.setHeader('Content-Type', 'application/json');
+
+    // res.send(JSON.stringify({ a: 1 }, null, 3));
+
+    res.json(users.find(user => user.id === id))
+})
+
+app.listen(8888, () => {
     console.log('Starting server at port 8888')
 })
