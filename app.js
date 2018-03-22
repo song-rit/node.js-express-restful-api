41c6cb31557d032a9703008b7e717414d8e77e3b
awidcha <song.cpe@gmail.com>
2018-03-22 21:45:57 +0700

    update get post put delete

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 const users = require('./db/user')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.get('/getUsers', function(req, res) {
+app.get('/users', function(req, res) {
     res.json(users)
 })
 
-app.get('/getUserById/:id', (req, res) => {
+app.get('/users/:id', (req, res) => {
     // res.json(users.find(user => user.id === req.params.id))
-    var id = req.params.id
+    const id = req.params.id
     res.json(users.find(user => user.id === id))
 })
 
 app.get('/getUserByParam', (req, res) => {
     // wait for request param from get method
-    var id = req.query.id
+    const id = req.query.id
     
     // this is older format
     //var id = req.body('id')
@@ -28,6 +33,30 @@ app.get('/getUserByParam', (req, res) => {
     // res.send(JSON.stringify({ a: 1 }, null, 3));
 
     res.json(users.find(user => user.id === id))
+})
+
+app.post('/users', (req, res) => {
+    const user = req.body
+    users.push(user)
+    res.setHeader('Content-Type', 'application/json');
+    res.status(201)
+        .send(JSON.stringify({ status:  "success"}, null, 3));
+})
+
+app.put('/users/:id', (req, res) => {
+    const id = req.params.id 
+    const updateIndex = users.findIndex(user => user.id === id)
+    const user = req.body
+    res.json(Object.assign(users[updateIndex], user))
+
+})
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params.id
+    const deleteIndex = users.findIndex(user => user.id === id)
+    users.splice(deleteIndex, 1)
+    res.status(204).send()
+
 })
 
 app.listen(8888, () => {
