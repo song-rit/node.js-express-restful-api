be4b07fa88ec84d332cc761634a74b74951aaa57
awidcha <song.cpe@gmail.com>
2019-02-20 11:32:32 +0700

    add code

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+// const request = require('request')
+const sync_request = require('sync-request')
 
 const app = express()
-const users = require('./db/user')
+const PORT = 8888
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -11,95 +13,70 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.get('/users', function(req, res) {
-    res.json(users)
-})
-
-app.get('/users/:id', (req, res) => {
-    // res.json(users.find(user => user.id === req.params.id))
-    const id = req.params.id
-    res.json(users.find(user => user.id === id))
-})
-
-app.post('/users', (req, res) => {
-    const user = req.body
-    users.push(user)
-    res.setHeader('Content-Type', 'application/json');
-    res.status(201)
-        .send(JSON.stringify({ status:  "success"}, null, 3));
-})
-
-app.put('/users/:id', (req, res) => {
-    const id = req.params.id 
-    const updateIndex = users.findIndex(user => user.id === id)
-    const user = req.body
-    res.json(Object.assign(users[updateIndex], user))
-})
-
-app.delete('/users/:id', (req, res) => {
-    const id = req.params.id
-    const deleteIndex = users.findIndex(user => user.id === id)
-    users.splice(deleteIndex, 1)
-    res.status(204).send()
-})
-
-app.get('/getUsers', (req, res) => {
+app.get('/sss_collection_photo', (req, res) => {
+    
     // wait for request param from get method
-    const id = req.query.id
-    
-    // this is older format
-    //var id = req.body('id')
+    const contract_no = req.query.contract_no
+    console.log('contract_no: ', contract_no)
+
+    var json_result = {
+        'contract_no': contract_no
+    }
 
     res.setHeader('Content-Type', 'application/json');
 
-    // res.send(JSON.stringify({ a: 1 }, null, 3));
-    const result = users
-    res.json(result)
+    // create json body
+    var body = {
+        "user_name": "adminfx|SCS|G2iOJxmBmQPHhFhL/+7+C3JSQflDl8x+ev642DrGFdQ=",
+        "data": {
+            "contract_no": [
+                contract_no
+            ]
+        }
+    }
+    // request http
+    var syncRes = sync_request('POST', 'http://localhost:9253/api/collection/getSSSPhotoFieldByContractNo', {
+      json: body,
+    });
+    
+    var responseString = syncRes.getBody('utf8')
+    var responseJson = JSON.parse(responseString);
+    res.json(responseJson)
 })
 
-app.get('/getUserById', (req, res) => {
+app.get('/sss_collection_gps', (req, res) => {
     // wait for request param from get method
-    const id = req.query.id
-    
-    // this is older format
-    //var id = req.body('id')
+    const contract_no = req.query.contract_no
+    console.log('contract_no: ', contract_no)
+
+    var json_result = {
+        'contract_no': contract_no
+    }
 
     res.setHeader('Content-Type', 'application/json');
 
-    // res.send(JSON.stringify({ a: 1 }, null, 3));
-    const result = users.find(user => user.id === id)
-    res.json(result)
+    // create json body
+    var body = {
+        "user_name": "adminfx|SCS|G2iOJxmBmQPHhFhL/+7+C3JSQflDl8x+ev642DrGFdQ=",
+        "data": {
+            "contract_no": [
+                contract_no
+            ]
+        }
+    }
+
+    // request http
+    var syncRes = sync_request('POST', 'http://localhost:9253/api/collection/getSSSGpsFieldByContractNo', {
+      json: body,
+    });
+   
+    var responseString = syncRes.getBody('utf8')
+    var responseJson = JSON.parse(responseString);
+    res.json(responseJson)
+
 })
 
-app.post('/addUser', (req, res) => {
-    
-    const user = req.body
-    users.push(user)
-    res.setHeader('Content-Type', 'application/json');
-    res.status(201)
-        .send(JSON.stringify({ status:  "success"}, null, 3));
-})
 
-app.post('/updateUser', (req, res) => {
-    const user = req.body
-    const updateId = user.id
-    const updateIndex = users.findIndex(user => user.id === updateId)
-
-    // users.push(user)
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({status: "success"}, null, 3))
-})
-
-app.get('/deleteUserById', (req, res) => {
-    const deleteId = req.query.id
-    const deleteIndex = users.findIndex(user => user.id === deleteId)
-    users.splice(deleteIndex, 1)
-    res.setHeader('Content-Type', 'application/json');
-    // res.json(users.splice(deleteIndex, 1))
-
-    res.send(JSON.stringify({ status:  "success"}, null, 3));
-})
-
-app.listen(8888, () => {
-    console.log('Starting server at port 8888')
+app.listen(PORT, () => {
+    console.log('Starting server at port ' . PORT)
 })
